fa68482210acab7781a1344da2db78cb2227d849
Shooshpanius <admin@in-da-house.ru>
2016-08-17 18:41:36 +0300

    Init win logs

--- app/controllers/receiver/win_eventlog_controller.rb
+++ app/controllers/receiver/win_eventlog_controller.rb
@@ -5,7 +5,7 @@ class Receiver::WinEventlogController < ApplicationController
   def in
 
     WinEventLog.create(
-        comment: params[:Hostname]
+        comment: params.to_s
     )
 
     render :nothing => true
