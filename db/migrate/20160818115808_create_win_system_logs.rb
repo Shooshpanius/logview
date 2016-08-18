fd02e69d9b2ea836d50e2101fb79508f1f206780
Shooshpanius <admin@in-da-house.ru>
2016-08-18 17:26:10 +0300

    Collect windows system log

--- db/migrate/20160818115808_create_win_system_logs.rb
+++ db/migrate/20160818115808_create_win_system_logs.rb
@@ -4,10 +4,12 @@ class CreateWinSystemLogs < ActiveRecord::Migration[5.0]
 
       t.string    'AccountName'
       t.string    'AccountType'
+      t.string    'action'
       t.string    'BitlockerUserInputTime'
       t.string    'BootType'
       t.string    'Category'
       t.string    'Channel'
+      t.string    'controller'
       t.string    'DeviceName'
       t.string    'DeviceNameLength'
       t.string    'DeviceTime'
