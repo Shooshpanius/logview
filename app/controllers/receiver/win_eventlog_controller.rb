1f17611c158b5e1905c6109e9f5ed19637f823e5
Shooshpanius <admin@in-da-house.ru>
2016-08-19 11:39:46 +0300

    Collect windows  logs

--- app/controllers/receiver/win_eventlog_controller.rb
+++ app/controllers/receiver/win_eventlog_controller.rb
@@ -4,7 +4,7 @@ class Receiver::WinEventlogController < ApplicationController
 
   def in
     params.permit!
-    params[:AccountName] = params[:AccountName].force_encoding('utf-8').encode
+    params[:AccountName] = params[:AccountName].force_encoding('iso-8859-1').encode('utf-8')
     log_channel_event = params[:Channel].to_s
     case log_channel_event
       when "System"
