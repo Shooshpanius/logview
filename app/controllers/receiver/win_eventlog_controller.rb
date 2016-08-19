9d4e4c836c8538aff5626a8ab30d62e4ec56c6dc
Shooshpanius <admin@in-da-house.ru>
2016-08-19 08:42:37 +0300

    Collect windows system log

--- app/controllers/receiver/win_eventlog_controller.rb
+++ app/controllers/receiver/win_eventlog_controller.rb
@@ -6,20 +6,26 @@ class Receiver::WinEventlogController < ApplicationController
     log_channel_event = params[:Channel].to_s
     case log_channel_event
       when "System"
-        event_id = params[:EventID].to_i
-        case event_id
-          when 0,1,2,4,6,10,18,20,25,27,30,32,33,34,35,36
-            params.permit!
-            params[:AccountName] = ''
-            WinSystemLog.create(params)
-          else
-            WinEventLog.create(
-                :event => params.to_xml,
-                :event_id => params[:EventID],
-                :channel => params[:Channel]
-            )
+        begin
+          WinSystemLog.create(params)
+        rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
         end
 
+      when "Security"
+        begin
+          WinSecurityLog.create(params)
+        rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+        end
       else
         WinEventLog.create(
             :event => params.to_xml,
