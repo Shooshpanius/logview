857ad502125a8f2a503a460b26f0b4f791b2653e
Shooshpanius <admin@in-da-house.ru>
2016-08-22 12:39:46 +0300

    Collect windows  logs

--- app/controllers/receiver/win_eventlog_controller.rb
+++ app/controllers/receiver/win_eventlog_controller.rb
@@ -13,29 +13,36 @@ class Receiver::WinEventlogController < ApplicationController
     log_channel_event = params[:Channel].to_s
     case log_channel_event
       when "System"
-#        begin
+       begin
           WinSystemLog.create(params)
-#        rescue
-          # WinEventLog.create(
-          #     :event => params.to_xml,
-          #     :event_id => params[:EventID],
-          #     :channel => params[:Channel].to_s + "_rescue"
-          # )
-#        end
+       rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+       end
 
       when "Security"
-#        begin
+       begin
           WinSecurityLog.create(params)
-#        rescue
-          # WinEventLog.create(
-          #     :event => params.to_xml,
-          #     :event_id => params[:EventID],
-          #     :channel => params[:Channel].to_s + "_rescue"
-          # )
-#        end
+       rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+       end
       when "Application"
-
-        WinApplicationLog.create(params)
+        begin
+          WinApplicationLog.create(params)
+        rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+        end
 
       else
         WinEventLog.create(
