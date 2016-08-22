173f39dbe6c3220f3794e63c54a81f982465c52f
Shooshpanius <admin@in-da-house.ru>
2016-08-22 10:48:59 +0300

    Collect windows  logs

--- db/migrate/20160822073636_create_win_application_logs.rb
+++ db/migrate/20160822073636_create_win_application_logs.rb
@@ -2,6 +2,27 @@ class CreateWinApplicationLogs < ActiveRecord::Migration[5.0]
   def change
     create_table :win_application_logs do |t|
 
+      t.text      'Category'
+      t.text      'Channel'
+      t.text      'EventID'
+      t.datetime  'EventReceivedTime'
+      t.datetime  'EventTime'
+      t.text      'EventType'
+      t.text      'Hostname'
+      t.text      'Keywords'
+      t.text      'Message'
+      t.text      'Opcode'
+      t.text      'ProcessID'
+      t.text      'RecordNumber'
+      t.text      'Severity'
+      t.text      'SeverityValue'
+      t.text      'SourceModuleName'
+      t.text      'SourceModuleType'
+      t.text      'SourceName'
+      t.text      'Task'
+      t.text      'ThreadID'
+      t.text      'win_eventlog'
+
       t.timestamps
     end
   end
