65d08af37f2bd1d6f18d6f921077f8bcb352aa5a
Shooshpanius <admin@in-da-house.ru>
2016-08-23 15:01:47 +0300

    Collect dovecot  logs

--- app/controllers/receiver/unix_log_controller.rb
+++ app/controllers/receiver/unix_log_controller.rb
@@ -9,17 +9,19 @@ class Receiver::UnixLogController < ApplicationController
     case source_name
       when "imap-login"
 
-        log_array = params[:Message].split(":")[2].split(",")
-
-        DovecotLog.create(
-            :user     => log_array[0][7..-2],
-            :method   => log_array[1][8..-1],
-            :rip      => log_array[2][5..-1],
-            :lip      => log_array[3][5..-1],
-            :mpid     => log_array[4][6..-1],
-            :security => log_array[5][1..-1],
-            :session  => log_array[6][10..-3]
-        )
+        begin
+          log_array = params[:Message].split(":")[2].split(",")
+          DovecotLog.create(
+              :user     => log_array[0][7..-2],
+              :method   => log_array[1][8..-1],
+              :rip      => log_array[2][5..-1],
+              :lip      => log_array[3][5..-1],
+              :mpid     => log_array[4][6..-1],
+              :security => log_array[5][1..-1],
+              :session  => log_array[6][10..-3]
+          )
+        rescue
+        end
 
       else
 
