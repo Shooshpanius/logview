00c078701755d1c1b59a1152955e3db1045e57de
Shooshpanius <admin@in-da-house.ru>
2016-08-23 17:55:21 +0300

    Collect logs base init

--- app/controllers/receiver/win_eventlog_controller.rb
+++ app/controllers/receiver/win_eventlog_controller.rb
@@ -13,7 +13,7 @@ class Receiver::WinEventlogController < ApplicationController
     log_channel_event = params[:Channel].to_s
     case log_channel_event
       when "System"
-       # begin
+       begin
 
           if params[:EventID] == 20
             error_code = params[:errorCode]
@@ -25,16 +25,16 @@ class Receiver::WinEventlogController < ApplicationController
           WinSystemLog.create(params)
           Host.where(:Hostname => params[:Hostname]).first_or_create
 
-       # rescue
-       #    WinEventLog.create(
-       #        :event => params.to_xml,
-       #        :event_id => params[:EventID],
-       #        :channel => params[:Channel].to_s + "_rescue"
-       #    )
-       # end
+       rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+       end
 
       when "Security"
-       # begin
+       begin
 
         if params[:EventID] == 5447
           action = params[:Action]
@@ -45,24 +45,24 @@ class Receiver::WinEventlogController < ApplicationController
 
           WinSecurityLog.create(params)
           Host.where(:Hostname => params[:Hostname]).first_or_create
-       # rescue
-       #    WinEventLog.create(
-       #        :event => params.to_xml,
-       #        :event_id => params[:EventID],
-       #        :channel => params[:Channel].to_s + "_rescue"
-       #    )
-       # end
+       rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+       end
       when "Application"
-        # begin
+        begin
           WinApplicationLog.create(params)
           Host.where(:Hostname => params[:Hostname]).first_or_create
-        # rescue
-        #   WinEventLog.create(
-        #       :event => params.to_xml,
-        #       :event_id => params[:EventID],
-        #       :channel => params[:Channel].to_s + "_rescue"
-        #   )
-        # end
+        rescue
+          WinEventLog.create(
+              :event => params.to_xml,
+              :event_id => params[:EventID],
+              :channel => params[:Channel].to_s + "_rescue"
+          )
+        end
 
       else
         WinEventLog.create(
