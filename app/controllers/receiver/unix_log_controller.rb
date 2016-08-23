cb03139d18bdf11a3807e3e6763d8e36576b5e9f
Shooshpanius <admin@in-da-house.ru>
2016-08-23 13:29:37 +0300

    Collect dovecot  logs

--- app/controllers/receiver/unix_log_controller.rb
+++ app/controllers/receiver/unix_log_controller.rb
@@ -10,7 +10,7 @@ class Receiver::UnixLogController < ApplicationController
       when "imap-login"
 
         UnixTestSysLog.create(
-            :msg => params[:Message].split(":").split(",").to_xml
+            :msg => params[:Message].split(":").split(",")[2].to_json
         )
 
       else
