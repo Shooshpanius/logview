853e2113b454cde5b81aaa316ae39805b8ecbfaf
Shooshpanius <admin@in-da-house.ru>
2016-09-01 08:12:28 +0300

    Collect logs base init

--- app/controllers/receiver/win_eventlog_controller.rb
+++ app/controllers/receiver/win_eventlog_controller.rb
@@ -23,6 +23,12 @@ class Receiver::WinEventlogController < ApplicationController
             params[:ErrorCode] = error_code
           end
 
+          if params[:EventID] == 16962
+            default_sd_string = params['Default-SD-String:']
+            params.delete('Default-SD-String:')
+            params[:Default-SD-String] = default_sd_string
+          end
+
           if params[:EventID] == 40960
             error = params[:Error]
             params.delete('Error')
